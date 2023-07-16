d2f3ca5db682ee846cadfabe601d49c9e198b74f
Nowel13 <nowel13@uma.es>
2023-07-16 19:49:19 +0200

    Removed date column from randomForest and now use holiday column to predict

--- py_scripts/random_forest/random_forest.py
+++ py_scripts/random_forest/random_forest.py
@@ -5,9 +5,9 @@ from sklearn.ensemble import RandomForestRegressor
 import datetime
 import matplotlib.pyplot as plt
 
-# data = pd.read_csv("../result_files/BasePrediction.txt", sep=' ')
-data = pd.read_csv("../result_files/BasePrediction2.txt", sep=' ')
-# data = pd.read_csv("../result_files/transformed_data.txt", sep=' ')
+# data = pd.read_csv("../../result_files/BasePrediction.txt", sep=' ')
+# data = pd.read_csv("../../result_files/BasePrediction2.txt", sep=' ')
+data = pd.read_csv("../../result_files/transformed_data.txt", sep=' ')
 
 ###########################################
 ### Split data into features and labels ###
@@ -22,7 +22,7 @@ labels = np.array(data['mean_kwh'])
 # Si trabajamos con BasePrediction, usar la primera linea, si es BasePrediction2, la segunda:
 # features = data.drop(['Time', 'sum_kwh', 'count_users', 'mean_kwh'], axis = 1)
 # features = data.drop(['mean_kwh'], axis = 1)
-features = data.drop(['mean_kwh', 'prediction'], axis = 1)
+features = data.drop(['mean_kwh', 'prediction', 'date'], axis = 1)
 
 # Guardamos los nombres de las columnas para más tarde:
 feature_list = list(features.columns)
@@ -59,43 +59,43 @@ print('Accuracy (2 decimales):', round(accuracy, 2), '%.')
 print('Accuracy:', accuracy, '%.')
 
 
-init_date = datetime.datetime(year=2009, month=1, day=1, hour=0)
+# init_date = datetime.datetime(year=2009, month=1, day=1, hour=0)
 
-def calculate_date(row):
-    return init_date + datetime.timedelta(days=int(row["day"]))
+# def calculate_date(row):
+#     return init_date + datetime.timedelta(days=int(row["day"]))
 
-def prepare_data(hour_filter):
-    data1 = data[data["hour"]==hour_filter]
-    labels1 = np.array(data[data['hour']==hour_filter]['mean_kwh'])
-    true_dates = data1.apply(calculate_date, axis=1)
-    true_data = pd.DataFrame(data= {
-        "date": true_dates,
-        "kwh": labels1
-    })
-    test_days = test_features[:, feature_list.index('day')]
-    test_hours = test_features[:, feature_list.index('hour')]
-    test_data = pd.DataFrame(data= {
-        "day": test_days,
-        "hour": test_hours,
-        "prediction": predictions
-    })
-    test_data = test_data[test_data["hour"]==hour_filter]
-    test_data["date"] = test_data.apply(calculate_date, axis=1)
-    test_data = test_data.drop(["day","hour"], axis=1)
-    plt.figure()
-    plt.plot(true_data['date'], true_data['kwh'], 'b-', label = 'kwh')
-    plt.plot(test_data['date'], test_data['prediction'], 'ro', label = 'prediction')
-    plt.xticks(rotation = 60)
-    plt.legend()
-    plt.xlabel('Date')
-    plt.ylabel('Mean Kwh')
-    plt.title('Actual and Predicted Values')
+# def prepare_data(hour_filter):
+#     data1 = data[data["hour"]==hour_filter]
+#     labels1 = np.array(data[data['hour']==hour_filter]['mean_kwh'])
+#     true_dates = data1.apply(calculate_date, axis=1)
+#     true_data = pd.DataFrame(data= {
+#         "date": true_dates,
+#         "kwh": labels1
+#     })
+#     test_days = test_features[:, feature_list.index('day')]
+#     test_hours = test_features[:, feature_list.index('hour')]
+#     test_data = pd.DataFrame(data= {
+#         "day": test_days,
+#         "hour": test_hours,
+#         "prediction": predictions
+#     })
+#     test_data = test_data[test_data["hour"]==hour_filter]
+#     test_data["date"] = test_data.apply(calculate_date, axis=1)
+#     test_data = test_data.drop(["day","hour"], axis=1)
+#     plt.figure()
+#     plt.plot(true_data['date'], true_data['kwh'], 'b-', label = 'kwh')
+#     plt.plot(test_data['date'], test_data['prediction'], 'ro', label = 'prediction')
+#     plt.xticks(rotation = 60)
+#     plt.legend()
+#     plt.xlabel('Date')
+#     plt.ylabel('Mean Kwh')
+#     plt.title('Actual and Predicted Values')
 
 
-# Podemos mostrar los graficos de los valores de las 24 horas por separado, pero por cuestiones de consumo de memoria, mostraremos solo 5:
-for x in range(0,5):
-    prepare_data(x)
+# # Podemos mostrar los graficos de los valores de las 24 horas por separado, pero por cuestiones de consumo de memoria, mostraremos solo 5:
+# for x in range(0,5):
+#     prepare_data(x)
 
-plt.show()
+# plt.show()
 
 
